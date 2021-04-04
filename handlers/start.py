fbc8a88ca120a4377a33ec5d28dbf32745b01ae3
Agustin Nunez <anunez@gmail.com>
2021-04-04 18:50:33 +0000

    minor start

--- handlers/start.py
+++ handlers/start.py
@@ -4,4 +4,4 @@ from handlers.system import silence_keeper
 @silence_keeper
 def start(bot, update):
 #def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Hi! I am a fleaMarketbot!\n Type /help to find out about my capabilities.')
+    update.message.reply_text("Hi! I'm CanaryMakersbot!\n Type /help to find out about my capabilities.")
